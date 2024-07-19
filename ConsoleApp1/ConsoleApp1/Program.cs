15d9985f79fe0c3f7c76b1522c314f2d79864a0a
JIAtype <148043337+JIAtype@users.noreply.github.com>
2024-07-19 09:17:50 +0800

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+SingleNumber singleNumber = new SingleNumber();
+while (true)
+{
+    singleNumber.click();
+    Thread.Sleep(10000);
+}
+Console.ReadLine(); 
+
+internal class SingleNumber
+{
+    [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+    public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtions, uint dwExtraInfo);
+    private const uint leftD = 0x02;
+    private const uint leftU = 0x04;
+    public void click()
+    {
+        mouse_event(leftD, 0, 0, 0, 0);
+        mouse_event(leftU, 0, 0, 0, 0);
+    }
+}
